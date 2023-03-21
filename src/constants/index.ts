8b156dc6eb5a14c2c6c33811f437b6cfab15d493
seytakaevRus <seytakaev@bk.ru>
2023-03-22 00:02:40 +0300

    Fix wrong install with argon2

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const EMAIL_ALREADY_EXIST = 'Такая почта уже существует';
+export const NEED_AUTH = 'Требуется авторизация';
+export const PASSWORDS_NOT_EQUAL = 'Пароли не совпадают';
+export const WRONG_CREDENTIALS = 'Неправильный логин или пароль';
+export const SERVER_ERROR = 'Ошибка сервера';
+export const NEED_DATA = 'Данные не указаны';
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,33 +1,41 @@
 import { verify, sign, Secret, SigningKeyCallback } from 'jsonwebtoken';
 import { promisify } from 'util';
-import argon2 from 'argon2';
-import { randomBytes } from 'crypto';
+import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
+
+import {
+  EMAIL_ALREADY_EXIST,
+  NEED_AUTH,
+  NEED_DATA,
+  PASSWORDS_NOT_EQUAL,
+  SERVER_ERROR,
+  WRONG_CREDENTIALS,
+} from '../constants/index';
 
 import pull from '../db/db.config';
 
-const randomBytesAsync = promisify(randomBytes);
 const verifyAsync = promisify(verify) as (
   token: string,
-  secretOrPublicKey: Secret
+  secretOrPublicKey: Secret,
 ) => Promise<SigningKeyCallback>;
 
 const authController = {
   async signIn(req: Request, res: Response) {
     const { password, email } = req.body;
 
-    if (!password || !email) {
-      res.status(400).send('Данные не указаны');
-    }
-
     try {
+      if (!password || !email) {
+        throw new Error(NEED_DATA);
+      }
+
       const [userData] = await pull.query(
         'SELECT id, email, firstname, lastname, password FROM diplom.users WHERE email = ?',
-        [email]
+        [email],
       );
 
+      // @ts-ignore
       if (userData.length === 0) {
-        res.status(400).send('Неправильный логин или пароль');
+        throw new Error(WRONG_CREDENTIALS);
       }
 
       const {
@@ -35,21 +43,23 @@ const authController = {
         id,
         firstname: firstName,
         lastname: lastName,
+        // @ts-ignore
       } = userData[0];
       const fullName = `${firstName} ${lastName}`;
 
       if (!process.env.ACCESS_SECRET_KEY) {
-        throw new Error('ACCESS_SECRET_KEY must be defined');
+        console.error('ACCESS_SECRET_KEY должен быть определен');
+        throw new Error(SERVER_ERROR);
       }
 
-      if (await argon2.verify(passwordHash, password)) {
+      if (await bcrypt.compare(password, passwordHash)) {
         const token = sign(
           {
             email,
             id,
             fullName,
           },
-          process.env.ACCESS_SECRET_KEY
+          process.env.ACCESS_SECRET_KEY,
         );
 
         res.cookie('token', token, { httpOnly: true });
@@ -60,33 +70,40 @@ const authController = {
           email,
         });
       } else {
-        res.status(400).send('Неправильный логин или пароль');
+        throw new Error(WRONG_CREDENTIALS);
       }
-    } catch (error) {
-      console.error(error);
+    } catch (error: any) {
+      if ('message' in error) {
+        if (error.message === SERVER_ERROR) {
+          res.status(500).send(error.message);
+        } else {
+          res.status(400).send(error.message);
+        }
+      }
     }
   },
   async signUp(req: Request, res: Response) {
     const { firstName, lastName, email, password, passwordConfirm } = req.body;
 
-    if (!firstName || !lastName || !email || !password || !passwordConfirm) {
-      res.status(400).send('Данные не указаны');
-    }
-
     try {
-      const [emails] = await pull.query(
-        'SELECT email FROM users WHERE email = ?',
-        [email]
-      );
-
-      if (emails.length > 0) {
-        res.status(400).send('Такая почта уже существует');
-      } else if (password !== passwordConfirm) {
-        res.status(400).send('Пароли не совпадают');
+      if (!firstName || !lastName || !email || !password || !passwordConfirm) {
+        throw new Error(NEED_DATA);
       }
 
-      const salt = await randomBytesAsync(32);
-      const passwordHash = await argon2.hash(password, { salt });
+      const [emails] = await pull.query(
+        'SELECT email FROM users WHERE email = ?',
+        [email],
+      );
+
+      // @ts-ignore
+      if (emails.length > 0) {
+        throw new Error(EMAIL_ALREADY_EXIST);
+      } else if (password !== passwordConfirm) {
+        throw new Error(PASSWORDS_NOT_EQUAL);
+      }
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      const passwordHash = await bcrypt.hash(password, salt);
 
       const [rows] = await pull.query('INSERT INTO users SET ?', {
         firstName,
@@ -97,46 +114,62 @@ const authController = {
       const fullName = `${firstName} ${lastName}`;
 
       if (!process.env.ACCESS_SECRET_KEY) {
-        throw new Error('ACCESS_SECRET_KEY must be defined');
+        console.error('ACCESS_SECRET_KEY должен быть определен');
+        throw new Error(SERVER_ERROR);
       }
 
       const token = sign(
         {
           email,
+          // @ts-ignore
           id: rows.insertId,
           fullName,
         },
-        process.env.ACCESS_SECRET_KEY
+        process.env.ACCESS_SECRET_KEY,
       );
 
       res.cookie('token', token, { httpOnly: true });
 
       res.status(201).send({
+        // @ts-ignore
         id: rows.insertId,
         fullName,
         email,
       });
-    } catch (error) {
-      console.error(error);
+    } catch (error: any) {
+      if ('message' in error) {
+        if (error.message === SERVER_ERROR) {
+          res.status(500).send(error.message);
+        } else {
+          res.status(400).send(error.message);
+        }
+      }
     }
   },
   async whoAmI(req: Request, res: Response) {
     const { token } = req.cookies;
 
-    if (!token) {
-      res.status(401).send('Требуется авторизация');
-    }
-
-    if (!process.env.ACCESS_SECRET_KEY) {
-      throw new Error('ACCESS_SECRET_KEY must be defined');
-    }
-
     try {
+      if (!token) {
+        throw new Error(NEED_AUTH);
+      }
+
+      if (!process.env.ACCESS_SECRET_KEY) {
+        console.error('ACCESS_SECRET_KEY должен быть определен');
+        throw new Error(SERVER_ERROR);
+      }
+
+      // TODO: Получать из токена ID пользователя и по нему из базы доставать пользователя.
       const userData = await verifyAsync(token, process.env.ACCESS_SECRET_KEY);
       res.status(200).send(userData);
-    } catch (error) {
-      // TODO: посмотреть как обрабатывать ошибки в Express.
-      console.error(error);
+    } catch (error: any) {
+      if ('message' in error) {
+        if (error.message === SERVER_ERROR) {
+          res.status(500).send(error.message);
+        } else {
+          res.status(400).send(error.message);
+        }
+      }
     }
   },
 };
